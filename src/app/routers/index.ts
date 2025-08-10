efec6530f3243092c9bb9d515130321a1079421f
obidyhasan <obidyhasan@gmail.com>
2025-08-10 09:35:56 +0600

    include stats modules APIs

--- src/app/routers/index.ts
+++ src/app/routers/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import { UserRouters } from "../modules/user/user.route";
+import { AuthRouters } from "../modules/auth/auth.route";
+import { ProductRouters } from "../modules/product/product.route";
+import { CartRouter } from "../modules/cart/cart.route";
+import { OrderRouters } from "../modules/order/order.route";
+import { OTPRouter } from "../modules/otp/otp.route";
+import { StatsRouters } from "../modules/stats/stats.route";
 
 export const router = Router();
 
@@ -7,6 +13,30 @@ const moduleRouters = [
   {
     path: "/user",
     router: UserRouters,
+  },
+  {
+    path: "/auth",
+    router: AuthRouters,
+  },
+  {
+    path: "/product",
+    router: ProductRouters,
+  },
+  {
+    path: "/cart",
+    router: CartRouter,
+  },
+  {
+    path: "/order",
+    router: OrderRouters,
+  },
+  {
+    path: "/otp",
+    router: OTPRouter,
+  },
+  {
+    path: "/stats",
+    router: StatsRouters,
   },
 ];
 
